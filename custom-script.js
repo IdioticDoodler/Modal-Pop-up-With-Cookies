04cb519ac258b03b14531867610f86f23f5380fb
IdioticDoodler <39472759+IdioticDoodler@users.noreply.github.com>
2018-05-30 10:35:15 +0800

    Update custom-script.js

--- custom-script.js
+++ custom-script.js
@@ -6,15 +6,16 @@
 		}
 		
 		if ( navigator.cookieEnabled ){
-		  // code for newsletter modal popup
+		  // code for newsletter modal popup, when it doesn't have yet the cookie the pop-up will show
 		  if ( Cookies.get('newsletter') == undefined ) {
 			setTimeout(function() {
 			  //  show the modal after 5 seconds
 			  jQuery("body").css("overflow-y" , "hidden");
 			  jQuery("#popup-wrapper").delay(100).fadeIn();
 			}, 1000);
-			  
+			  //when you close the pop-up it will create the cookie
 			jQuery(".close").click(function(){
+				//the 0.001 is equivalent to 2mins, 1 = 1day
 				Cookies.set('newsletter', 'default', { expires: 0.001 });
 				jQuery("#popup-wrapper").hide();
 			});
